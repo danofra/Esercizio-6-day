e28ecfd56b56dcfef443ae2ff224035ace01aec2
Liborio Daniele Frasca <frascaliboriodaniele@gmail.com>
2024-03-11 16:56:56 +0100

    Primo esercizio

--- src/exceptions/InvalidIndex.java
+++
@@ -1,9 +0,0 @@
-package exceptions;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-public class InvalidIndex extends Exception {
-    private static final Logger log = LoggerFactory.getLogger(InvalidIndex.class);
-    public InvalidIndex(int number) {
-        log.error("L'indice " + number + " inserito non è valido. Riprova.");
-    }
-}
--- src/exceptions/InvalidNumber.java
+++
@@ -1,9 +0,0 @@
-package exceptions;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-public class InvalidNumber extends Exception {
-    private static final Logger log = LoggerFactory.getLogger(InvalidNumber.class);
-    public InvalidNumber(int number) {
-        log.error("Il numero " + number + " inserito non è valido. Riprova.");
-    }
-}
